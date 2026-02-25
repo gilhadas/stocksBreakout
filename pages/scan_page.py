1e312edc0725c326ea25b0a33f28ee0389b513d4
Gil Hadas <gil.hadas@gmail.com>
2026-02-25 17:53:16 +0200

    no need to , worksing at last

--- pages/scan_page.py
+++ pages/scan_page.py
@@ -468,8 +468,7 @@ def render_scan_page():
             watchlist_name = st.selectbox("Watchlist", wl_keys, index=default_idx)
         else:
             watchlist_name = None
-            import utils
-            st.warning(f"No watchlists in input is_cloud: {utils.is_cloud()} ")
+            st.warning("No watchlists in input/")
 
     with col_filter:
         quality_filter = st.selectbox("Filter", ["GOLD", "PREMIUM+", "HIGH+", "ALL"], index=2)
